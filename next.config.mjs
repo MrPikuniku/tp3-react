d898aac47d598c95c69e067b551e12a33f26d2d5
Mamourou SANGARE <ms284034@iut21.u-bourgogne.fr>
2024-10-09 17:30:02 +0200

    question 2.1 : Page d'accueil

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.starbucks.fr',
+      },
+    ],
+  }};
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,222 @@
 import Image from "next/image";
+import { PRODUCTS_CATEGORY_DATA} from "@arthur.eudeline/starbucks-tp-kit/data";
+import { BreadCrumbs } from "@arthur.eudeline/starbucks-tp-kit/components/breadcrumbs";
+import { SectionContainer } from "@arthur.eudeline/starbucks-tp-kit/components/section-container";
+import { ProductGridLayout } from "@arthur.eudeline/starbucks-tp-kit/components/products/product-grid-layout";
+import { ProductCardLayout } from "@arthur.eudeline/starbucks-tp-kit/components/products/product-card-layout";
+import { Button } from "@arthur.eudeline/starbucks-tp-kit/components/button";
+
+const categories = PRODUCTS_CATEGORY_DATA;
+console.log(categories);
 
 export default function Home() {
+  
+
+
   return (
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <BreadCrumbs 
+          items={[
+            { 
+              label: 'Accueil', 
+              url:'#1' 
+            }
+            
+          ]}
+        
+        />
+
+
+        <SectionContainer>
+          {
+            categories[0].name  
+          }({categories[0].products.length})
+          <ProductGridLayout products={categories[0].products}>
+	          {(product) => (
+              <ProductCardLayout
+                product={product}
+                button={
+                  <Button variant={"ghost"} fullWidth>
+                    Ajouter au panier
+                  </Button>
+                }
+            />)}
+          </ProductGridLayout>
+        </SectionContainer>
+
+
+        <SectionContainer>
+        {
+            categories[1].name  
+          }({categories[1].products.length})
+          <ProductGridLayout products={categories[1].products}>
+	          {(product) => (
+              <ProductCardLayout
+                product={product}
+                button={
+                  <Button variant={"ghost"} fullWidth>
+                    Ajouter au panier
+                  </Button>
+                }
+            />)}
+          </ProductGridLayout>
+        </SectionContainer>
+
+
+        <SectionContainer>
+          {
+            categories[2].name  
+          }({categories[2].products.length})
+          <ProductGridLayout products={categories[2].products}>
+	          {(product) => (
+              <ProductCardLayout
+                product={product}
+                button={
+                  <Button variant={"ghost"} fullWidth>
+                    Ajouter au panier
+                  </Button>
+                }
+            />)}
+          </ProductGridLayout>
+        </SectionContainer>
+
+        <SectionContainer>
+          {
+            categories[3].name  
+          }({categories[3].products.length})
+          <ProductGridLayout products={categories[3].products}>
+	          {(product) => (
+              <ProductCardLayout
+                product={product}
+                button={
+                  <Button variant={"ghost"} fullWidth>
+                    Ajouter au panier
+                  </Button>
+                }
+            />)}
+          </ProductGridLayout>
+        </SectionContainer>
+
+        <SectionContainer>
+          {
+            categories[4].name  
+          }({categories[4].products.length})
+          <ProductGridLayout products={categories[4].products}>
+	          {(product) => (
+              <ProductCardLayout
+                product={product}
+                button={
+                  <Button variant={"ghost"} fullWidth>
+                    Ajouter au panier
+                  </Button>
+                }
+            />)}
+          </ProductGridLayout>
+        </SectionContainer>
+
+        <SectionContainer>
+          {
+            categories[5].name  
+          }({categories[5].products.length})
+          <ProductGridLayout products={categories[5].products}>
+	          {(product) => (
+              <ProductCardLayout
+                product={product}
+                button={
+                  <Button variant={"ghost"} fullWidth>
+                    Ajouter au panier
+                  </Button>
+                }
+            />)}
+          </ProductGridLayout>
+        </SectionContainer>
+
+        <SectionContainer>
+          {
+            categories[6].name  
+          }({categories[6].products.length})
+          <ProductGridLayout products={categories[6].products}>
+	          {(product) => (
+              <ProductCardLayout
+                product={product}
+                button={
+                  <Button variant={"ghost"} fullWidth>
+                    Ajouter au panier
+                  </Button>
+                }
+            />)}
+          </ProductGridLayout>
+        </SectionContainer>
+
+        <SectionContainer>
+          {
+            categories[7].name  
+          }({categories[7].products.length})
+          <ProductGridLayout products={categories[7].products}>
+	          {(product) => (
+              <ProductCardLayout
+                product={product}
+                button={
+                  <Button variant={"ghost"} fullWidth>
+                    Ajouter au panier
+                  </Button>
+                }
+            />)}
+          </ProductGridLayout>
+        </SectionContainer>
+
+        <SectionContainer>
+          {
+            categories[8].name  
+          }({categories[8].products.length})
+          <ProductGridLayout products={categories[8].products}>
+	          {(product) => (
+              <ProductCardLayout
+                product={product}
+                button={
+                  <Button variant={"ghost"} fullWidth>
+                    Ajouter au panier
+                  </Button>
+                }
+            />)}
+          </ProductGridLayout>
+        </SectionContainer>
+
+        <SectionContainer>
+          {
+            categories[9].name  
+          }({categories[9].products.length})
+          <ProductGridLayout products={categories[9].products}>
+	          {(product) => (
+              <ProductCardLayout
+                product={product}
+                button={
+                  <Button variant={"ghost"} fullWidth>
+                    Ajouter au panier
+                  </Button>
+                }
+            />)}
+          </ProductGridLayout>
+        </SectionContainer>
+
+        <SectionContainer>
+          {
+            categories[10].name  
+          }({categories[10].products.length})
+          <ProductGridLayout products={categories[10].products}>
+	          {(product) => (
+              <ProductCardLayout
+                product={product}
+                button={
+                  <Button variant={"ghost"} fullWidth>
+                    Ajouter au panier
+                  </Button>
+                }
+            />)}
+          </ProductGridLayout>
+        </SectionContainer>
+
+        
       </main>
   );
 }
